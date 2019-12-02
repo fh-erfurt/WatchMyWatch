9c34290eeaf1032091ff7a2a23e17cf9d20976ba
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2019-12-02 13:05:17 +0100

    Added more basic methods to Order

--- src/main/java/de/watchmywatch/classes/Order.java
+++ src/main/java/de/watchmywatch/classes/Order.java
@@ -18,34 +18,59 @@ public class Order
     Date getOrderDate(){
     return this->ordered;
     }
+    // TODO
     void setOrderDate(Date date){
-    // zusätzlich Logik?
-    this ordered = date;
+    // additional logic needed here?
+    this->ordered = date;
     }
+
     Date getShipDate(){
     return this->shipped;
     }
+    void setShipDate(Date date){
+    this->shipped =date;
+    }
+
     String getID(){
     return this->id;
     }
+    // No SetID -> ID is set only in constructor
+
     String getAddressID(){
     return this->addressID;
     }
+    bool setAddressID(int newAddressID){
+    this->addressID = newAddressID;
+    }
+
+    // Todo: Create Enum Class OrderStatus
     Enum getOrderStatus(){
     return this->OrderStatus;
     }
+    void setOrderStatus(Enum newOrderStatus){
+    this->OrderStatus = newOrderStatus;
+    }
+
     Enum getShippingStatus(){
     return this->ShippingStatus;
     }
+    void setShippingStatus(Enum newShippingStatus){
+    this->ShippingStatus =newShippingStatus;
+    }
+
     BigDecimal getTotal(){
     return this->total;
     }
+    // TODO: Create Method for calculating the sum of Prices of Orderitems to Total price of Order
+
     Shoppingcart getShoppingcart(){
     // Referenz?
     return this->shoppingcart;
     }
+    // TODO: Method for setting a reasonable Shoppingcart to Order
+
     Payment getPayment(){
     return this->payment;
     }
-
+    // TODO: Method for setting Payment
 }
