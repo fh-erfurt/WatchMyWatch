8945bbe6c96a6ee9489ca0ed0ab945ae74611920
Michael Hopp <56825650+Maffotter@users.noreply.github.com>
2019-12-02 12:24:16 +0100

    Added basic methods to Class Order

--- src/main/java/de/watchmywatch/classes/Order.java
+++ src/main/java/de/watchmywatch/classes/Order.java
@@ -14,4 +14,38 @@ public class Order
     private BigDecimal total;
     private Shoppingcart shoppingcart;
     private Payment payment;
+
+    Date getOrderDate(){
+    return this->ordered;
+    }
+    void setOrderDate(Date date){
+    // zusätzlich Logik?
+    this ordered = date;
+    }
+    Date getShipDate(){
+    return this->shipped;
+    }
+    String getID(){
+    return this->id;
+    }
+    String getAddressID(){
+    return this->addressID;
+    }
+    Enum getOrderStatus(){
+    return this->OrderStatus;
+    }
+    Enum getShippingStatus(){
+    return this->ShippingStatus;
+    }
+    BigDecimal getTotal(){
+    return this->total;
+    }
+    Shoppingcart getShoppingcart(){
+    // Referenz?
+    return this->shoppingcart;
+    }
+    Payment getPayment(){
+    return this->payment;
+    }
+
 }
