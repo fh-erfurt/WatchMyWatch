3b3430baf061d20fd6bc56975ea0238fcb8fb6a6
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2019-12-08 21:05:50 +0100

    Update ManagerWatch.java

--- src/main/java/de/watchmywatch/Uhrenverwaltung/ManagerWatch.java
+++ src/main/java/de/watchmywatch/Uhrenverwaltung/ManagerWatch.java
@@ -1,5 +1,16 @@
 package de.watchmywatch.Uhrenverwaltung;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ManagerWatch
 {
+    private static Map<Integer, Watch> watchMap = new HashMap<Integer, Watch>();
+    private static int idCounter = 1;
+
+    public boolean addWatch(){return false};
+    public boolean removeWatch(){return false};
+    public BigDecimal getPriceWithExtraCost(){return new BigDecimal(1);};
+    public BigDecimal getPriceWithoutExtraCost(){return new BigDecimal(1);};
 }
