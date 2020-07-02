124621cd5cba9433c3078235e72d344556badcdb
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-07-02 14:33:42 +0200

    Delete PersonRepository.java

--- src/main/java/de/watchmywatch/repository/storage/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package de.watchmywatch.repository.storage;
-
-import de.watchmywatch.model.AccountManagment.Person;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
