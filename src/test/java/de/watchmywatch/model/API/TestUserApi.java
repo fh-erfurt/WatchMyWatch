942b22f876312793a0b5b9fc30fc89dc1bb251f1
TKSpectro <45234980+TKSpectro@users.noreply.github.com>
2020-08-04 20:24:34 +0200

    Create TestUserApi.java still has bugs

---
+++ src/test/java/de/watchmywatch/model/API/TestUserApi.java
@@ -0,0 +1,64 @@
+package de.watchmywatch.model.API;
+
+import de.watchmywatch.model.AccountManagment.User;
+import de.watchmywatch.model.Helper.Address;
+import de.watchmywatch.model.OrderManagment.Shoppingcart;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class TestUserApi {
+    @Test
+    public void whenRequestGet_thenOK() {
+        given().port(8086)
+                .when().request("GET", "/api/users")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void whenRequestGetWithId_thenOK() {
+        given().port(8086).when().request("GET", "/api/users/1")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void whenPost_thenCreatedAndCorrect() {
+        Address address = new Address("streetName", "cityName", "stateName", "zipName");
+        User user = new User("firstname", "lastname", "email", "password",
+                "12301230123", address, LocalDate.of(1998, 9, 23), address, new Shoppingcart());
+        given().port(8086).contentType("application/json").body(user)
+                .when().post("/api/users")
+                .then()
+                .statusCode(201)
+                .body("firstname", equalTo(user.getFirstname()))
+                .body("lastname", equalTo(user.getLastname()))
+                .body("email", equalTo(user.getEmail()))
+                .body("phone", equalTo(user.getPhone()));
+    }
+
+    @Test
+    public void whenPut_thenCorrect() {
+        Address address = new Address("streetName", "cityName", "stateName", "zipName");
+        given().port(8086)
+                .contentType("application/json")
+                .body(address)
+                .when()
+                .put("/api/users/10")
+                .then()
+                .statusCode(200)
+                .body("street", equalTo(address.getStreet()))
+                .body("city", equalTo(address.getCity()))
+                .body("state", equalTo(address.getState()))
+                .body("zip", equalTo(address.getZip()));
+    }
+
+    @Test
+    public void whenDelete_thenCorrect() {
+        given().port(8086).contentType("application/json")
+                .when().delete("/api/users/13")
+                .then().statusCode(204);
+    }
+}
